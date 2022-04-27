b64a670b2ff47748787684e54d4c5481abc9c5d4
James Albert Harritz Grech <jameshg69@gmail.com>
2022-04-27 10:39:02 +0200

    messageemulator klasse

--- 2_sem_eksamen_bravo/MessageEmulator.cs
+++ 2_sem_eksamen_bravo/MessageEmulator.cs
@@ -9,23 +9,13 @@ namespace _2_sem_eksamen_bravo
 {
     static class MessageEmulator
     {
-        public static string path = Directory.GetCurrentDirectory();
-
         public static void EmulateSendSms(string message)
         {
-            //Husk at lave en mappe for de filer der bliver gemt
-            using (StreamWriter w = new StreamWriter(path.Remove(path.IndexOf(@"\bin")) + @"\log.html"))
-            {
 
-            }
         }
         public static void EmulateSendEmail(string message)
         {
-            //Husk at lave en mappe for de filer der bliver gemt
-            using (StreamWriter w = new StreamWriter(path.Remove(path.IndexOf(@"\bin")) + @"\log.txt"))
-            {
 
-            }
         }
     }
 }
