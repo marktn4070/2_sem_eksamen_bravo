0c19bace9ad3fc3293bc632d4928e4f931d507d8
James Albert Harritz Grech <jameshg69@gmail.com>
2022-04-27 10:39:02 +0200

    messageemulator klasse

---
+++ 2_sem_eksamen_bravo/MessageEmulator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace _2_sem_eksamen_bravo
+{
+    static class MessageEmulator
+    {
+        public static void EmulateSendSms(string message)
+        {
+
+        }
+        public static void EmulateSendEmail(string message)
+        {
+
+        }
+    }
+}
