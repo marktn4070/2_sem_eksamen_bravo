3fcde6652bcded0aaff2673c9d0f02b80e07a293
James Albert Harritz Grech <14974@student.eadania.dk>
2022-05-01 21:17:39 +0200

    basic email html

--- 2_sem_eksamen_bravo/MessageEmulator.cs
+++ 2_sem_eksamen_bravo/MessageEmulator.cs
@@ -11,20 +11,28 @@ namespace _2_sem_eksamen_bravo
     {
         public static string path = Directory.GetCurrentDirectory();
 
-        public static void EmulateSendSms(string message)
+        public static void EmulateSendSms(string headline, string subheadline, string message)
         {
             //Husk at lave en mappe for de filer der bliver gemt
-            using (StreamWriter w = new StreamWriter(path.Remove(path.IndexOf(@"\bin")) + @"\log.html"))
+            using (StreamWriter w = new StreamWriter(path.Remove(path.IndexOf(@"\bin")) + @"\log" + DateTime.Now.ToString("yyyyddMMHHmmss") + ".txt"))
             {
-
+                w.Write(headline + "\n \n" + subheadline + "\n \n" + message);
             }
         }
-        public static void EmulateSendEmail(string message)
+        public static void EmulateSendEmail(string headline, string subheadline, string message)
         {
             //Husk at lave en mappe for de filer der bliver gemt
-            using (StreamWriter w = new StreamWriter(path.Remove(path.IndexOf(@"\bin")) + @"\log.txt"))
+            using (StreamWriter w = new StreamWriter(path.Remove(path.IndexOf(@"\bin")) + @"\log" + DateTime.Now.ToString("yyyyddMMHHmmss") + ".html"))
             {
+                w.Write(@" <!DOCTYPE html>
+                <html>
+                <body>
 
+                <h1> " + headline + @" </h1>
+                <h2> " + subheadline + @" </h2>
+                <p> " + message + @"</p>
+                </body>
+                </html> ");
             }
         }
     }
