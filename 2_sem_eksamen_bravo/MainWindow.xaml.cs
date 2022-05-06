7d7d27437f7aeba40c0d719cfb219a1d964648eb
Mark <snapstil1eren@gmail.com>
2022-05-06 09:12:34 +0200

    Test

--- 2_sem_eksamen_bravo/MainWindow.xaml.cs
+++ 2_sem_eksamen_bravo/MainWindow.xaml.cs
@@ -16,7 +16,7 @@ using System.Windows.Shapes;
 namespace _2_sem_eksamen_bravo
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for MainWindow.xamlff
     /// </summary>
     public partial class MainWindow : Window
     {
