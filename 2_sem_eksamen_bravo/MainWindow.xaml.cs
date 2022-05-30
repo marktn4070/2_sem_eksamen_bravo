23de322424bbbae8b80c27d00539851c49cb982d
Mark <snapstil1eren@gmail.com>
2022-05-30 20:15:30 +0200

    Random

--- 2_sem_eksamen_bravo/MainWindow.xaml.cs
+++ 2_sem_eksamen_bravo/MainWindow.xaml.cs
@@ -21,7 +21,7 @@ namespace _2_sem_eksamen_bravo
 /////// HEAD
     /// Interaction logic for MainWindow.xaml
 //////
-    /// Interaction logic for MainWindow.xamlerferfeddscdscs
+    /// Interaction logic for MainWindow.xamlerferfedd
 ////// master
     /// Interaction logic for MainWindow.xaml
 ////// Temporary merge branch 2
