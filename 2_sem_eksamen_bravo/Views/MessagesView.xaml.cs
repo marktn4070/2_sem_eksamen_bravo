c9a35d01a72a97e497972c8fd44c6b526652f735
Kevin Bjørn <73933092+KevinDania@users.noreply.github.com>
2022-06-02 11:16:16 +0200

    .

--- 2_sem_eksamen_bravo/Views/MessagesView.xaml.cs
+++ 2_sem_eksamen_bravo/Views/MessagesView.xaml.cs
@@ -88,38 +88,38 @@ namespace _2_sem_eksamen_bravo.Views
         {
             if (startDate_Search.SelectedDate != null && endDate_Search.SelectedDate != null)
             {
-            DateTime SD = startDate_Search.SelectedDate.Value;
-            string startDate = SD.ToString("yyyy/MM/dd");
-            DateTime ED = endDate_Search.SelectedDate.Value;
-            string endDate = ED.ToString("yyyy/MM/dd");
-            try
-            {
-                if (DateTime.Compare(SD, ED) <= 0)
+                DateTime SD = startDate_Search.SelectedDate.Value;
+                string startDate = SD.ToString("yyyy/MM/dd");
+                DateTime ED = endDate_Search.SelectedDate.Value;
+                string endDate = ED.ToString("yyyy/MM/dd");
+                try
                 {
-                    DataTable dt = SQL.SearchMessage(startDate, endDate);
-                    datagrid_message.ItemsSource = dt.DefaultView;
-                    //time_Search.Background = Brushes.Transparent;
-                    //string name_txt = name_txt;
-                    ////name_txt = "";
+                    if (DateTime.Compare(SD, ED) <= 0)
+                    {
+                        DataTable dt = SQL.SearchMessage(startDate, endDate);
+                        datagrid_message.ItemsSource = dt.DefaultView;
+                        //time_Search.Background = Brushes.Transparent;
+                        //string name_txt = name_txt;
+                        ////name_txt = "";
 
-                    int Search_items = datagrid_message.Items.Count;
+                        int Search_items = datagrid_message.Items.Count;
 
-                    if (Search_items == 0)
-                    {
-                    Search_message.Content = "Der er ingen resultater på din søgningen";
-                    }
-                    else if (Search_items == 1)
-                    {
-                    Search_message.Content = Search_items + " resultat af søgningen fra '" + startDate + "' til '" + endDate + "'";
-                    Search_message.Foreground = Brushes.Black;
-                    }
-                    else if (Search_items > 1)
-                    {
-                        Search_message.Content = Search_items + " resultater af søgningen fra '" + startDate + "' til '" + endDate + "'";
-                        Search_message.Foreground = Brushes.Black;
-                    }
-                    //ClearDataBtn.Visibility = Visibility.Visible;
-                    //SearchDataBtn.Visibility = Visibility.Hidden;
+                        if (Search_items == 0)
+                        {
+                            Search_message.Content = "Der er ingen resultater på din søgningen";
+                        }
+                        else if (Search_items == 1)
+                        {
+                            Search_message.Content = Search_items + " resultat af søgningen fra '" + startDate + "' til '" + endDate + "'";
+                            Search_message.Foreground = Brushes.Black;
+                        }
+                        else if (Search_items > 1)
+                        {
+                            Search_message.Content = Search_items + " resultater af søgningen fra '" + startDate + "' til '" + endDate + "'";
+                            Search_message.Foreground = Brushes.Black;
+                        }
+                        //ClearDataBtn.Visibility = Visibility.Visible;
+                        //SearchDataBtn.Visibility = Visibility.Hidden;
                     }
                     else
                     {
@@ -148,41 +148,5 @@ namespace _2_sem_eksamen_bravo.Views
 
         }
         #endregion
-
-
-
-        SqlConnection host = new SqlConnection(ConfigurationManager.ConnectionStrings["host"].ConnectionString);
-
-        public void GetMMessage_not_done()
-        {
-            //List<string> Headline = SQL.GetMMessage_2();
-            var products = new List<Product>();
-
-            for (int i = 0; i < 5; i++)
-            {
-                int j = i + 1;
-                //products.Add(new Product("Product 1", 205.46, "/Assets/1.jpg"));
-                products.Add(new Product("Headline " + j, "Subheadline", "Time"));
-            }
-
-            //if (products.Count > 0)
-                //ListViewProducts.ItemsSource = products;
-        }
-    }
-
-
-
-    public class Product
-    {
-        public string Headline_Btn { get; set; }
-        public string Subheadline { get; set; }
-        public string Time { get; set; }
-
-        public Product(string headline_btn, string subheadline, string time)
-        {
-            Headline_Btn = headline_btn;
-            Subheadline = subheadline;
-            Time = time;
-        }
     }
 }
